8806346939c7a966d4f0387106a54042757ca1a0
Prasanth Sivakumar <k2142172@login3.pri.rosalind2.alces.network>
2021-04-27 19:04:10 +0100

    add steps to submission file

---
+++ submission.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+#SBATCH --partition=brc
+#SBATCH --time=00:10:00
+#SBATCH --mem=50M
+#SBATCH --job-name=rp_submit
+#SBATCH --verbose
+#SBATCH --output=/scratch/users/k2142172/tests//rp_submit_%A.out
+
+# script exits if return value of a command is not zero
+set -e
+# print shell input lines as they are read for debugging
+set -v
+# prevents output redirection from overwriting existing files
+set -o noclobber
+
+args=("$@")
+
+sample_table=${args[0]}
+config=${args[1]}
+
+fastqc_step=/scratch/users/k2142172/scripts/rp_fastqc.sh
+multiqc_step=/scratch/users/k2142172/scripts/rp_multiqc.sh
+alignment_step=/scratch/users/k2142172/scripts/rp_alignment.sh
+
+export config=$config
+export sample_table=$sample_table
+
+#sbatch $fastqc_step
+sbatch $multiqc_step
+
