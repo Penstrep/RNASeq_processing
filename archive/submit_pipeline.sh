621f03c4ba53f42fdc53fab4280af4fdc89fbaa0
prasanth.sivakumar@kcl.ac.uk <k2142172@login4.pri.rosalind2.alces.network>
2021-09-21 10:45:13 +0100

    move old no multiqc to archive

---
+++ archive/submit_pipeline.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+#SBATCH --partition=brc
+#SBATCH --time=00:10:00
+#SBATCH --mem=50M
+#SBATCH --job-name=rp_submit
+#SBATCH --verbose
+#SBATCH --output=/scratch/users/k2142172/tests/rp_submit_%A.out
+
+# script exits if return value of a command is not zero
+set -e
+# print shell input lines as they are read for debugging
+set -v
+# prevents output redirection from overwriting existing files
+set -o noclobber
+
+args=("$@")
+
+sample_table=${args[0]}
+config=${args[1]}
+
+fastqc_step=/scratch/users/k2142172/scripts/pipeline/rp_fastqc.sh
+multiqc_step=/scratch/users/k2142172/scripts/pipeline/rp_multiqc.sh
+alignment_step=/scratch/users/k2142172/scripts/pipeline/rp_alignment.sh
+index_step=/scratch/users/k2142172/scripts/pipeline/rp_index_bams.sh
+counts_step=/scratch/users/k2142172/scripts/pipeline/rp_counts_matrix.sh
+
+export config=$config
+export sample_table=$sample_table
+
+sbatch --dependency=singleton $fastqc_step
+sbatch --dependency=singleton $multiqc_step
+sbatch --dependency=singleton $alignment_step
+sbatch --dependency=singleton $index_step
+sbatch --dependency=singleton $counts_step
