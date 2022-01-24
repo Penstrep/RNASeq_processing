720f81ff5265a0c686384edc7b724811a7d00139
Prasanth Sivakumar <prasanth.sivakumar@kcl.ac.uk>
2022-01-24 10:14:41 +0000

    run with config params

--- rp_counts_qc.sh
+++ rp_counts_qc.sh
@@ -1,21 +1,39 @@
-#Rscript -e "rmarkdown::render('rp_normalise_counts.Rmd', \
-#params=list(counts_matrix = '/scratch/users/k2142172/outputs/ramsey_s303/gene_expression/STAR/ramsey_s303_gene_counts.tab', \
-#drop_samples=TRUE, dropped_samples='Positive.control_S37 Negative.control_S38 Undetermined_S0 NAT.4_S4'))"
+#!/bin/bash
 
-#fl=/scratch/users/k2142172/scripts/pipeline/rp_normalise_counts.Rmd
+#SBATCH --partition=brc
+#SBATCH --time=00:30:00
+#SBATCH --mem=6G
+#SBATCH --job-name=r_pipeline
+#SBATCH --verbose
+#SBATCH --output=/scratch/users/k2142172/tests/rp_counts_qc.out
+
+# import config variables
+. ./$config
+
+# create output dir if necessary, and redirect log and err files there
+exec >${out_dir}/gene_expression/${project}_rp_counts_qc.out 2>${out_dir}/gene_expression/${project}_rp_counts_qc.out
 
 # rp_normalise_counts.Rmd
-markdown_file=$1
+markdown_file=/scratch/users/k2142172/scripts/pipeline/rp_normalise_counts.Rmd
 
 # gene_counts.tab
-counts_matrix=$2
+counts_matrix=${out_dir}/gene_expression/STAR/${project}_gene_counts.tab
 
 # TRUE/FALSE - default FALSE
-drop_samples=$3
+drop_samples=${drop_samples}
 
 # 'sample1 sample2 sample3' - default ''
-dropped_samples=$4
+dropped_samples=${dropped_samples}
 
-Rscript -e "rmarkdown::render('$markdown_file', output_file='/scratch/users/k2142172/tests/test.html', params=list(counts_matrix = '$2', drop_samples='$3', dropped_samples='$4'))"
+# output html
+output_file=${out_dir}/gene_expression/${project}_counts_qc.html
+
+Rscript -e "rmarkdown::render('$markdown_file', \
+    output_file='$output_file', \
+    params=list(counts_matrix='$counts_matrix', \
+        project='${project}', \
+        out_dir='${out_dir}', \
+        drop_samples='$drop_samples',\
+        dropped_samples='$dropped_samples'))"
 
 
